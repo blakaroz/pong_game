d5664e5d95adafa56c46c9abcd5deff99077bbb3
blakaro <144008100+blakaroz@users.noreply.github.com>
2023-12-03 19:43:46 +0100

    Initial commit

---
+++ ball.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(0,0)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        y_new = self.ycor()+self.y_move
+        x_new = self.xcor()+self.x_move
+        self.goto(x_new,y_new)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
---
+++ paddle.py
@@ -0,0 +1,19 @@
+import turtle
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self,y_cor, x_cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(y_cor,x_cor)
+
+
+    def go_up(self):
+        new_y= self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    def go_down(self):
+        new_y= self.ycor() - 20
+        self.goto(self.xcor(), new_y)
